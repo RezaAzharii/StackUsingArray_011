c87252dd237efe8dbf5f52d27a9731eb96f62fcc
RezaAzharii <rezaazhari867@gmail.com>
2023-06-15 17:56:27 +0700

    menambahkan switch case pada program utama

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -67,6 +67,29 @@ int main() {
 		string input;
 		getline(cin, input);
 		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nEnter an element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.POP();
+		case '3':
+			s.display();
+			break;
+		case '4':
+			return 0;
+		default:
+			cout << "\nInvalid choice." << endl;
+			break;
 
+		}
 	}
 }
